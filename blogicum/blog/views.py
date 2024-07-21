fb3c8db6f44cb983eabb3998b61b7a05953de661
dunno <116356493+dunnotoru@users.noreply.github.com>
2024-07-22 00:28:56 +0700

    a

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -12,26 +12,10 @@ def index(request):
                 pub_date__lte=timezone.now(),
                 category__is_published=True)\
         .order_by('id')[:5]
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Category
-from django.utils import timezone
-
-
-def index(request):
-    post_list = Post.objects.all()\
-        .select_related('category')\
-        .select_related('author')\
-        .filter(is_published=True,
-                pub_date__lte=timezone.now(),
-                category__is_published=True)\
-        .order_by('id')[:5]
 
     context = {
         'post_list': post_list
-        'post_list': post_list
     }
-
-    return render(request, 'blog/index.html', context)
 
     return render(request, 'blog/index.html', context)
 
@@ -47,21 +31,9 @@ def category_posts(request, category_slug):
                 is_published=True,
                 pub_date__lte=timezone.now())\
 
-    category = get_object_or_404(Category, slug=category_slug)
-    if not category.is_published:
-        return HttpResponseNotFound()
-
-    post_list = Post.objects.all()\
-        .select_related('category')\
-        .filter(category__slug=category_slug,
-                is_published=True,
-                pub_date__lte=timezone.now())\
-
     context = {
         'post_list': post_list
-        'post_list': post_list
     }
-
 
     return render(request, 'blog/category.html', context)
 
@@ -80,21 +52,3 @@ def post_detail(request, id):
         'post': post
     }
     return render(request, 'blog/detail.html', context)
-
-
-def post_detail(request, id):
-    post = get_object_or_404(Post, pk=id)
-
-    if post.pub_date > timezone.now():
-        return HttpResponseNotFound()
-    if not post.is_published:
-        return HttpResponseNotFound()
-    if not post.category.is_published:
-        return HttpResponseNotFound()
-
-    context = {
-        'post': post
-    }
-    return render(request, 'blog/detail.html', context)
-
-
